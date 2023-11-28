9b6280590ce15b32f11e16c78a92f41b009ff0a6
Ekaterina Zvereva <evzvereva1@gmail.com>
2023-11-28 21:57:32 +0500

    correction cache in get_description_image.py

--- get_description_image.py
+++ get_description_image.py
@@ -26,7 +26,7 @@ def load_image():
         return None
 
 
-@st.cache(ttl=24*3600)
+@st.cache_resource
 def loads_models():
     processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
     model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
